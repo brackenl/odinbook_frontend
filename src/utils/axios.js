9cd8b6b392720e4976d2ab230c053b24bfd61f8e
William Bracken <liam.bracken@gmail.com>
2020-12-16 16:50:23 +0000

    Prepare for deployment

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://odinbook-react.herokuapp.com/api",
 });
 
 export default instance;
